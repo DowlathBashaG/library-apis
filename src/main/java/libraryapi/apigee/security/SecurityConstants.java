320f48976037f8d937ea8328d6f7dea63e3805ef
Arsh ADMIN Ariz <dowlathbasha@gmail.com>
2020-05-25 20:45:53 +0530

    added space in bearer token in security constants

--- src/main/java/libraryapi/apigee/security/SecurityConstants.java
+++ src/main/java/libraryapi/apigee/security/SecurityConstants.java
@@ -9,6 +9,6 @@ public class SecurityConstants {
     public static final String SIGNING_SECRET = "MyApiSecret";
     public static final String NEW_USER_DEFAULT_PASSWORD = "Password123";
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_TOKEN_PREFIX = "Bearer";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
     public static final String NEW_USER_REGISTRATION_URL = "/v1/users";
 }
